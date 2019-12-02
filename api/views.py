33356ee06cbaae56f8c4b1df989624d9e770bb5b
Mike Yue <mikeyue97@gmail.com>
2019-12-01 20:12:01 -0800

    more bug fixes

--- api/views.py
+++ api/views.py
@@ -64,12 +64,14 @@ class CodeViewSet(viewsets.ModelViewSet):
         return Code.objects.filter(created_by=self.request.user)
     
     def create(self, request, *args, **kwargs):
+        offset = datetime.timezone(datetime.timedelta(hours=-8))
         target_lock = Lock.objects.get(lock_id=request.data['lock_id'])
+
         if not target_lock.code_set.filter(expired=False):
             if request.user in target_lock.users.all():
                 code_generator = secrets.SystemRandom()
                 code = code_generator.randint(0,9999)
-                offset = datetime.timezone(datetime.timedelta(hours=-8))
+                
                 temp = Code.objects.create(
                     code=code, 
                     lock=target_lock,
@@ -84,7 +86,23 @@ class CodeViewSet(viewsets.ModelViewSet):
         else:
             try:
                 existing_code = target_lock.code_set.get(expired=False)
-                return Response({"Message": "Your code is: {}".format(str(existing_code.code).zfill(4))}, status=status.HTTP_200_OK)
+                if existing_code.expiry_time <= datetime.datetime.now(offset):
+                    existing_code.expired = True
+                    existing_code.save()
+                    code_generator = secrets.SystemRandom()
+                    code = code_generator.randint(0,9999)
+                    
+                    temp = Code.objects.create(
+                        code=code, 
+                        lock=target_lock,
+                        expiry_time=request.data['expiry_time'], 
+                        created_by=request.user, 
+                        creation_time=datetime.datetime.now(offset),
+                        used_at_time=None,
+                        )
+                    return Response({"Message": "Your code is: {}".format(str(code).zfill(4))}, status=status.HTTP_200_OK)
+                else:
+                    return Response({"Message": "Your code is: {}".format(str(existing_code.code).zfill(4))}, status=status.HTTP_200_OK)
             except:
                 return Response({"Message": "Something bad happened in the server"}, status=status.HTTP_403_FORBIDDEN)
 
